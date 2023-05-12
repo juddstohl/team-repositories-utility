42ef682c092eab37ca9a93d2669cb7e1dded20cc
Judd Stohl <judd.stohl@ncino.com>
2023-05-12 14:04:30 -0400

    added Repo class and env variable

--- src/app.js
+++ src/app.js
@@ -9,13 +9,35 @@ const getBranchProtection = async (repo, branch) => {
 	);
 };
 
-const mainFunction = async () => {
-	const results = await getBranchProtection(
-		'force-integrations-plugin-lspus',
-		'release'
-	);
+const getRepos = () => {
+	const repos = process.env['REPOS'];
+	return repos.split(',');
+};
 
-	console.log(results);
+const Repo = class {
+	// originalProtection;
+
+	constructor(protection, repoName) {
+		this.originalProtection = protection;
+		this.repoName = repoName;
+	}
+
+	get isFrozen() {
+		return this.originalProtection.hasOwnProperty('restrictions');
+	}
+};
+
+const mainFunction = async () => {
+	const master = {};
+	const repos = getRepos();
+
+	for (let i = 0; i < repos.length; i++) {
+		const protection = await getBranchProtection(repos[i], 'release');
+		const repo = new Repo(protection, repos[i]);
+		master[repos[i]] = repo;
+	}
+
+	console.log(master[repos[0]].isFrozen);
 };
 
 mainFunction();
